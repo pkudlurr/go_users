dc45d1e1eeb6858a85729b6854e6acca0fea6017
Pradeep Kudlur <pradeep.kudlur@gmail.com>
2023-08-26 10:28:20 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pkudlurr/go_users
+
+go 1.21.0
+
+require github.com/pkudlurr/go_services v0.0.0-20230826165746-da2b4761ed6b
---
+++ test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "github.com/pkudlurr/go_services"
+)
+
+func main() {
+    fmt.Println(go_services.Greet("Pradeep"))
+}
+
